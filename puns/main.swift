1dcc42024b21775617a1fa88d0b2798e951b9670
Edward Loveall <edward@edwardloveall.com>
2015-11-01 10:34:35 -0500

    Initial Commit

---
+++ puns/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  puns
+//
+//  Created by Edward Loveall on 11/1/15.
+//  Copyright © 2015 Edward Loveall. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
