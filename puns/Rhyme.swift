ca7c4eee2fd388b4c36a5c91ce3bafb80bd59926
Edward Loveall <edward@edwardloveall.com>
2015-11-01 16:01:15 -0500

    Step 6, Create a Rhyme type

---
+++ puns/Rhyme.swift
@@ -0,0 +1,17 @@
+//
+//  Rhyme.swift
+//  puns
+//
+//  Created by Edward Loveall on 11/1/15.
+//  Copyright © 2015 Edward Loveall. All rights reserved.
+//
+
+import Foundation
+
+struct Rhyme {
+    var word: String,
+        freq: Int,
+        score: Int,
+        flags: String,
+        syllables: String
+}
