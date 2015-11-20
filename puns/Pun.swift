539735b91c1fb4df647aaf38a880384f14c07414
Edward Loveall <edward@edwardloveall.com>
2015-11-19 20:40:14 -0500

    Step 12, Create pun type for pun storage

---
+++ puns/Pun.swift
@@ -0,0 +1,14 @@
+//
+//  Pun.swift
+//  puns
+//
+//  Created by Edward Loveall on 11/19/15.
+//  Copyright © 2015 Edward Loveall. All rights reserved.
+//
+
+import Foundation
+
+struct Pun {
+    let originalPhrase: String
+    let punPhrase: String
+}
